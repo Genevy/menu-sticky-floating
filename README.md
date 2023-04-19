2e21ffdb074c0861a4728f56b0b477e4d3a29cc5
Genevy <evgeniy.striganoff@yandex.ru>
2023-04-19 13:00:44 +0300

    Add: index.html, style.css, script.js, .nojekyll . Upd: README.md

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# menu-sticky-floating
-Плавающее липкое меню сайта
+<div align="center"><h3>Плавающее липкое меню сайта</h3></div>
+
+##
+
+<br>
+<p align="center">
+Плавающее липкое горизонтальное меню сайта при скроллинге (прокрутке) страницы средствами HTML, CSS и JavaScript без jQuery и бубнов
+<br>
+<br>
+<p align="center">🔎 Explore on <a href="https://genevy.github.io/menu-sticky-floating/"><strong>GitHub Pages »</strong></a></p>
+<br>
+##
+####
+© Made with care for everyone's use by [Evgeny Striganov](https://github.com/genevy) 
---
+++ script.js
@@ -0,0 +1,29 @@
+/* Get the menu element */
+const headerMenu = document.querySelector('.header__nav');
+
+/* Set initial scroll position */
+let prevScrollPos = window.pageYOffset;
+
+/* Scroll detection function */
+function detectScrollDirection() {
+  const currentScrollPos = window.pageYOffset;
+
+  if (prevScrollPos > currentScrollPos) {
+    /* Scrolling up */
+    headerMenu.classList.remove('hidden_nav');
+  } else {
+    /* Scrolling down */
+    headerMenu.classList.add('hidden_nav');
+  }
+
+  /* Update previous scroll position */
+  prevScrollPos = currentScrollPos;
+}
+
+/* Add event listener to window object for scroll detection */
+window.addEventListener('scroll', detectScrollDirection);
+
+/* Adding the current year to footer */
+const currentYear = new Date().getFullYear();
+document.querySelector('.year').innerHTML = currentYear;
+
